f2b849cf19bab6b6841d6be8d0f90352dd02af14
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2025-01-13 00:24:54 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,269 @@
+/** Back-End development **/
+
+
+// Getting Started with NodeJS - 1
+/*
+Introduction to NodeJS.
+Why NodeJS is populor.
+Runtimes.
+Setting uo node.js.
+Blocking and Non-Blocking code.
+How nodejs works.
+Understanding Libuv.
+Whats is a Server?.
+Createing an HTTP Server.
+Handling Multiple Requests.
+Returning HTML as response.
+*/
+
+
+// Modules in NodeJS 
+/* 
+Understanding Module.
+ES6 Module.
+Types of Modules.
+Package manegers and NPM.
+Globle instralling using npm.
+Understanding package.json.
+Understanding versions.
+Removing mjs extensions.
+Understanding nvm.
+Reading command line input.
+File System Module-1.
+File System Module-2.
+File System Module-3.
+Path Module.
+*/
+
+
+// Exploring More Modules
+/* 
+Debugging Node.js.
+Debugging in VSCode.
+Email in Nodejs.
+Events in Nodejs.
+Testing API using Postman.
+res.end()Method.
+Creating Custom Events.
+Events-Driven Architecture.
+*/
+
+
+// Getting started With Express.js
+/* 
+What is Express.
+Creating Server using Express.
+How Express Works.
+More on Middlewares.
+Types of Requests.
+HTTP Headers and Status Codes.
+Serving HTML Pages.
+*/
+
+
+// Model View Controller-I
+/* 
+Getting started with web development.
+Understanding MVC.
+Understanding Project.
+Creatng View.
+Creating Controller.
+Creating Model.
+View Engine.
+Creating Layout.
+*/
+
+
+// Model View Controller-II
+/* 
+Working with Forms.
+Parsing Data.
+Validation Data.
+Validation Middleware.
+Using Express Validater.
+Fix Validation Errors.
+Updateing product.
+Submit Update Form.
+Deleting Data.
+Confirmation before deletion.
+Styling Views.
+*/
+
+
+// File, Upload, Session and Cookies
+/* 
+File Uploading - 1
+File Uploading - 2
+Understanding Session
+Registration Page.
+Login Page.
+Securing Application.
+Logout and Clearing Session.
+Understanding Cookie.
+Creating Cookie.
+Debugging Cookie Issue.
+Deleting Cookie.
+*/
+
+
+// E-comm Project Using Rest API.
+/* 
+Understanding API.
+Understanding RESt.
+Application of RESt.
+Getting Started with API Project.
+Creating Folder Structure.
+Setting up Routes for folder.
+Product controller.
+Post Request.
+Add product API.
+Get one Product.
+Filter Products.
+*/
+
+
+// Securing APIs
+/* 
+Understanding Security.
+User APIs.
+Testing User APIs.
+Basic Authentication.
+JWT.
+JWT Authentication-I.
+JWT Authentication-II.
+*/
+
+
+// Adding More Features to E-comm App
+/* 
+Rate Products.
+Testing rate APIs.
+Adding Cart Feature.
+testing cart feature.
+Deleting cart Item.
+API documentation.
+Using Swagger.
+Testing Swagger.
+Swager 3.0.
+Handling 404.
+CORS.
+CORS using headers.
+Cors using Library.
+*/
+
+
+// Handling Errors.
+/* 
+Creating Logger Middleware.
+Using Middleware Logger.
+Using winston logger
+Error Handling in Express.
+Appliaction Level Error handling.
+Customizing Error handling middleware.
+summarizing project.
+*/
+
+
+// Getting Started with Databases
+/* 
+Understanding Data.
+understanding Databases.
+Documents and Corrections.
+Documents CRUD in MongoDB-1.
+Documents CRUD in MongoDB-2.
+Seeting up MongoDB in Windows.
+*/
+
+
+// MongoDB with NodeJS-I
+/* 
+Connecting to MongoDB with NodeJS.
+User Operations with mongodb.
+Repository Pattern.
+Using Hash Password.
+Using env file.
+ProductRepository-I.
+ProductRepository-II.
+*/
+
+
+// MongoDB with NodeJS-II
+/* 
+Fix RateProduct problum.
+CartRepository.
+Updated Quantity.
+Modifying ID.
+Indexes.
+Creating Index.
+MongoDB Operators.
+projection operators.
+Aggegation Pipeline-I.
+Aggegation Pipeline-II.
+Translations in MongoDb-I.
+Translations in MongoDb-II.
+Translations in MongoDb-III.
+Translations in MongoDb-IV.
+*/
+
+
+// Working with Mongoose-I.
+/* 
+Understanding Mongoose.
+Connecting Using Mongoose.
+Creating Schemas.
+User Operations.
+Password Reset.
+Validations in Mongoose.
+Relationships in Mongoose.
+Testing One to Many.
+Many to Many Relationships.
+Testing M2M.
+Multiple Refernces.
+Testing multiple refernces.
+Mongoose Middlewares.
+Best practics with databases.
+MongoDB with Cloud.
+*/
+
+
+// Socket Programming
+/* 
+Introduction to Socket.
+Establishing Connencton.
+Creating Chat UI.
+Brodcasting message.
+User Indentification.
+Storing Chats in db.
+Loadling previus message.
+*/
+
+
+// Task Runners in NodeJS
+/* 
+Understanding TaskRunner.
+Setting up Guant.
+Guant Tasks-1.
+Guant Tasks-2.
+Setting up Gulp.
+Image Optimaztion using Gulp.
+*/
+
+// Deployment
+/* 
+Getting Started with deployment.
+Using Render for deployment.
+Using environment varialbes.
+Cloud with AWS.
+Lunching EC2 Instance.
+Deploying Nodejs on EC2.
+Using PM2 to run NodeJS.
+*/
+
+
+
+
+
+
+
+
+
